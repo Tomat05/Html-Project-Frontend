0591593974ec0ee0e9e45af5c16ce80276fd60af
Tomat05 <wetherill.t@outlook.com>
2022-10-30 21:10:37 +0000

    Fancy javascript moments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let vpHeight = document.documentElement.clientHeight;
+let width = document.documentElement.clientWidth;
 
 let layer0 = document.getElementById('l0');
 let layer1 = document.getElementById('l1');
@@ -7,14 +7,55 @@ let layer3 = document.getElementById('l3');
 let layer4 = document.getElementById('l4');
 let titles = document.getElementsByClassName('title');
 
-// Parallax effect
+let containers = document.getElementsByClassName('content');
+
+// Fade panels in
+function fadeIn(container) {
+    let duration = 0.3;
+    let interval = 5; //ms
+    let op = 0;
+    let iop = 1;
+    let timer = setInterval(function() {
+        if (op >= iop) {
+            op = iop;
+            clearInterval(timer)
+        }
+        container.style.opacity = op;
+        op += iop/((1000 / interval) * duration)
+    }, interval)
+}
+
+// Array of whether each panel is fading
+let fading = []
+for (let i = 0; i < containers.length; i++) {
+    fading[i] = false;
+}
+
+let page = 0;
+numPages = 2;
+
+// Scroll stuff
 window.addEventListener('scroll', function() {
-    let scroll = window.scrollX;
+    let scroll = window.scrollX;;
 
     layer1.style.left = scroll - (scroll / 32) + 'px';
     layer2.style.left = scroll - (scroll / 16) + 'px';
     layer3.style.left = scroll - (scroll / 8) + 'px';
     layer4.style.left = scroll - (scroll / 4) + 'px';
+
+    for (let i = 0; i < containers.length; i++) {
+        let bound = containers[i].getBoundingClientRect();
+
+        if (bound.right <= width && !fading[i] && (page - 1) == i) {
+            fading[i] = true;
+            fadeIn(containers[i]);
+        // } else if (bound.left >= width) {
+        //     // fading[i] = false;
+        //     containers[i].style.opacity = 0;
+        // }
+        // console.log(fading[i]);
+        }
+    }
 });
 
 // Change backgrounds on refresh for *spice*
